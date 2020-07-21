d8c519347f0d09b1232d22147e704574ca8ec989
James Mahler <james.mahler@gmail.com>
2020-07-20 21:58:30 -0500

    #2: side type

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,27 @@ pub mod error;
 pub mod parse_line;
 pub mod pattern;
 pub mod stitches;
+
+/// Represents the side of the line.
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+pub enum Side {
+    RS,
+    WS
+}
+
+impl Side {
+    /// Switch to the other side
+    ///
+    /// #Arguments
+    /// `in_round` - If the pattern is in the round
+    pub fn switch(self, in_round: bool) -> Side {
+        if in_round {
+            self
+        } else {
+            match self {
+                Side::RS => Side::WS,
+                Side::WS => Side::RS
+            }
+        }
+    }
+}
